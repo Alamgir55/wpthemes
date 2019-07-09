1e05eead5668c5e2859eb4b7e48e4d406471f3f0
Alamgir Hossain <alamigr555hossain@gmail.com>
2019-07-09 02:05:58 -0400

    initial commit

---
+++ wp-content/themes/wordstrap/footer.php
@@ -0,0 +1,10 @@
+<div class="container">
+        <hr>
+        <footer>
+            <p>&copy; 2019 Wordstrap</p>
+        </footer>
+</div>
+    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.js"></script>
+</body>
+</html>
