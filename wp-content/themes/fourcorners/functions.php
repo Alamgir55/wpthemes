af0477e591d338e41fd702a20746560b4c168225
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-07-26 23:31:03 -0400

    initial commit

--- wp-content/themes/fourcorners/functions.php
+++ wp-content/themes/fourcorners/functions.php
@@ -10,3 +10,51 @@ function fc_theme_support(){
     ));
 }
 add_action('after_setup_theme', 'fc_theme_support');
+
+function fc_init_widgets($id){
+    register_sidebar(array(
+        'name'  => 'Sidebar',
+        'id'    => 'sidebar',
+        'before_widget' => '<div class="sidebar-widget">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ));
+    register_sidebar(array(
+        'name'  => 'Footer 1',
+        'id'    => 'footer1',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name'  => 'Footer 2',
+        'id'    => 'footer2',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name'  => 'Footer 3',
+        'id'    => 'footer3',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ));
+    register_sidebar(array(
+        'name'  => 'Footer 4',
+        'id'    => 'footer4',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>'
+    ));
+}
+add_action('widgets_init', 'fc_init_widgets');
+
+require get_template_directory() . '/inc/customizer.php';
